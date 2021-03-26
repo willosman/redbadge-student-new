33149fafd0aa6dfe563c6f221fca9f826063852f
William Osman <wosman007@gmail.com>
2021-03-25 21:27:12 -0400

    adding classfetchonload

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import ClassFetchOnLoad from "./components/ClassFetchOnload";
 import FunctionFetchOnload from "./components/FunctionFetchOnload";
 
 function App() {
   return (
     <div>
       <FunctionFetchOnload />
+      <ClassFetchOnLoad />
     </div>
   );
 }
